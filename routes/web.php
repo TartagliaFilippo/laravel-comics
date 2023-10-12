ccc2fd60460f9da6c7f6efda4fe804c3a6938c80
filippo199600 <pippotartaglia@gmail.com>
2023-10-12 18:45:10 +0200

    printed cards with db

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,11 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('main-content');
-});
+    $products = config('db');
+
+    return view('main-content', compact('products'));
+})->name('main-content');
+
+Route::get('/card-detail', function () {
+    return view('card-detail');
+})->name('card-detail');
